e5b6dfe1b6c5dedc65f11e5b163f0aba45496614
Matt <mjruddy94@gmail.com>
2023-09-13 15:15:18 -0400

    add google analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
-import AboutSectionOne from "@/components/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
-import Blog from "@/components/Blog";
-import Brands from "@/components/Brands";
 import ScrollUp from "@/components/Common/ScrollUp";
 import Contact from "@/components/Contact";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
-import Pricing from "@/components/Pricing";
 import Testimonials from "@/components/Testimonials";
-import Video from "@/components/Video";
 import { Inter } from "@next/font/google";
+import Analytics from "analytics";
+import googleAnalytics from "@analytics/google-analytics";
 
 const inter = Inter({ subsets: ["latin"] });
+const analytics = Analytics({
+  app: "TRCPA",
+  plugins: [
+    googleAnalytics({
+      measurementIds: [process.env.NEXT_PUBLIC_ANALYTICS_ID],
+    }),
+  ],
+});
+
+analytics.page({ title: "home" });
 
 export default function Home() {
   return (
