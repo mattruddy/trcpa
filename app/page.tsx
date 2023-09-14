89b0e20ed8057c04888fd25eeb8c08f447564de5
Matt <mjruddy94@gmail.com>
2023-09-14 11:59:38 -0400

    add page view

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,8 @@ import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Testimonials from "@/components/Testimonials";
 import { Inter } from "@next/font/google";
-import Analytics from "analytics";
-import googleAnalytics from "@analytics/google-analytics";
 
 const inter = Inter({ subsets: ["latin"] });
-const analytics = Analytics({
-  app: "TRCPA",
-  plugins: [
-    googleAnalytics({
-      measurementIds: [process.env.NEXT_PUBLIC_ANALYTICS_ID],
-    }),
-  ],
-});
-
-analytics.page({ title: "home" });
 
 export default function Home() {
   return (
